b4e10e1b754a72517346c4cd706a2ee6c765d6cb
kaakaa <stooner.hoe@gmail.com>
2018-05-30 22:29:44 +0900

    Add test for password change mail

--- app/email_test.go
+++ app/email_test.go
@@ -8,9 +8,49 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/mattermost/mattermost-server/model"
+	// "github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/utils"
 )
+
+func TestSendPasswordChangeEmail(t *testing.T) {
+	th := Setup().InitBasic()
+	defer th.TearDown()
+
+	var email = "test@example.com"
+	var method = "method"
+	var locale = ""
+	var siteURL = "site_url"
+
+	//Delete all the messages before check the sample email
+	utils.DeleteMailBox(email)
+
+	th.App.SendPasswordChangeEmail(email, method, locale, siteURL)
+
+	var resultsMailbox utils.JSONMessageHeaderInbucket
+	err := utils.RetryInbucket(5, func() error {
+		var err error
+		resultsMailbox, err = utils.GetMailBox(email)
+		return err
+	})
+	if err != nil {
+		t.Log(err)
+		t.Log("No email was received, maybe due load on the server. Disabling this verification")
+	}
+	if err == nil && len(resultsMailbox) > 0 {
+		if !strings.ContainsAny(resultsMailbox[0].To[0], email) {
+			t.Fatal("Wrong To recipient")
+		} else {
+			if resultsEmail, err := utils.GetMessageFromMailbox(email, resultsMailbox[0].ID); err == nil {
+				b, err := ioutil.ReadFile("../tests/test-email-passwordChangeEmail.html")
+				require.NoError(t, err)
+				expected := strings.Split(string(b), "\n")			
+				for i, a := range strings.Split(resultsEmail.Body.HTML, "\r\n",){
+					assert.Equal(t, expected[i], a, fmt.Sprintf("Line %d is not match", i + 1))
+				}
+			}
+		}
+	}
+}
 
 func TestSendMfaActivateEmail(t *testing.T) {
 	th := Setup().InitBasic()
