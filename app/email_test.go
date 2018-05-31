9e45a00b77041b88ad274c790d6de4ff6dd2ace4
kaakaa <stooner.hoe@gmail.com>
2018-05-31 22:48:23 +0900

    Add test for change email mail

--- app/email_test.go
+++ app/email_test.go
@@ -53,6 +53,46 @@ func TestSendChangeUsernameEmail(t *testing.T) {
 	}
 }
 
+func TestSendEmailChangeEmail(t *testing.T) {
+	th := Setup().InitBasic()
+	defer th.TearDown()
+
+	var oldEmail = "test@example.com"
+	var newEmail = "new_email"
+	var locale = ""
+	var siteURL = "site_url"
+
+	//Delete all the messages before check the sample email
+	utils.DeleteMailBox(oldEmail)
+
+	th.App.SendEmailChangeEmail(oldEmail, newEmail, locale, siteURL)
+
+	var resultsMailbox utils.JSONMessageHeaderInbucket
+	err := utils.RetryInbucket(5, func() error {
+		var err error
+		resultsMailbox, err = utils.GetMailBox(oldEmail)
+		return err
+	})
+	if err != nil {
+		t.Log(err)
+		t.Fatal("No email was received, maybe due load on the server. Disabling this verification")
+	}
+	if err == nil && len(resultsMailbox) > 0 {
+		if !strings.ContainsAny(resultsMailbox[0].To[0], oldEmail) {
+			t.Fatal("Wrong To recipient")
+		} else {
+			if resultsEmail, err := utils.GetMessageFromMailbox(oldEmail, resultsMailbox[0].ID); err == nil {
+				b, err := ioutil.ReadFile("../tests/test-email-emailChangeEmail.html")
+				require.NoError(t, err)
+				expected := strings.Split(string(b), "\n")			
+				for i, a := range strings.Split(resultsEmail.Body.HTML, "\r\n",){
+					assert.Equal(t, expected[i], a, fmt.Sprintf("Line %d is not match", i + 1))
+				}
+			}
+		}
+	}
+}
+
 
 func TestSendSignInChangeEmail(t *testing.T) {
 	th := Setup().InitBasic()
