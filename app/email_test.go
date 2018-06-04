bc785a3d743fa0b93f4784ddf9f2403d5955c6a0
kaakaa <stooner.hoe@gmail.com>
2018-06-04 22:10:28 +0900

    Add test for deactivate account mail

--- app/email_test.go
+++ app/email_test.go
@@ -332,3 +332,42 @@ func TestSendMfaDeactivateEmail(t *testing.T) {
 		}
 	}
 }
+
+func TestSendDeactivateAccountEmail(t *testing.T) {
+	th := Setup().InitBasic()
+	defer th.TearDown()
+
+	var emailTo = "test@example.com"
+	var locale = ""
+	var siteURL = "http://site_url"
+
+	//Delete all the messages before check the sample email
+	utils.DeleteMailBox(emailTo)
+
+	th.App.SendDeactivateAccountEmail(emailTo, locale, siteURL)
+
+	var resultsMailbox utils.JSONMessageHeaderInbucket
+	err := utils.RetryInbucket(5, func() error {
+		var err error
+		resultsMailbox, err = utils.GetMailBox(emailTo)
+		return err
+	})
+	if err != nil {
+		t.Log(err)
+		t.Log("No email was received, maybe due load on the server. Disabling this verification")
+	}
+	if err == nil && len(resultsMailbox) > 0 {
+		if !strings.ContainsAny(resultsMailbox[0].To[0], emailTo) {
+			t.Fatal("Wrong To recipient")
+		} else {
+			if resultsEmail, err := utils.GetMessageFromMailbox(emailTo, resultsMailbox[0].ID); err == nil {
+				b, err := ioutil.ReadFile("../tests/test-email-deactivateAccountEmail.html")
+				require.NoError(t, err)
+				expected := strings.Split(string(b), "\n")			
+				for i, a := range strings.Split(resultsEmail.Body.HTML, "\r\n",){
+					assert.Equal(t, expected[i], a, fmt.Sprintf("Line %d is not match", i + 1))
+				}
+			}
+		}
+	}
+}
